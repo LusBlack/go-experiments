e692312c58f2e3a4aab077865cbdca8f70b60020
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-28 12:09:32 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	//initializes variables here
+	var smsSendingLimit int
+	var costPerSMS float64
+	var hasPermission bool
+	var username string
+
+	fmt.Printf(
+		"%v %f %v %q\n",
+		smsSendingLimit,
+		costPerSMS,
+		hasPermission,
+		username,
+	)
+}
